120b93b50fab1d3118318ef712dc488b098b0c2f
Chris4891 <kolar.kristijan84@gmail.com>
2023-10-11 21:22:31 +0500

    dfining the post method for stripe integration

--- core/views.py
+++ core/views.py
@@ -4,10 +4,20 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, View
+from django.views.generic import ListView, DetailView, View
 from django.shortcuts import redirect
-from .models import Item, Order
+from django.utils import timezone
+from .forms import CheckoutForm, CouponForm, RefundForm
+from .models import Item, OrderItem, Order, BillingAddress, Payment, Coupon, Refund, Category
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
+
+# Create your views here.
+import random
+import string
+import stripe
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 
@@ -25,6 +35,33 @@ class PaymentView(View):
             messages.warning(
                 self.request, "u have not added a billing address")
             return redirect("core:checkout")
+
+    def post(self, *args, **kwargs):
+        order = Order.objects.get(user=self.request.user, ordered=False)
+        token = self.request.POST.get('stripeToken')
+        amount = int(order.get_total() * 100)
+        
+        charge = stripe.Charge.create(
+            amount=amount,  # cents
+            currency="usd",
+            source=token
+        )
+        # create the payment
+        payment = Payment()
+        payment.stripe_charge_id = charge['id']
+        payment.user = self.request.user
+        payment.amount = order.get_total()
+        payment.save()
+
+        # assign the payment to the order
+        order.ordered = True
+        order.payment = payment
+        # TODO : assign ref code
+        order.ref_code = create_ref_code()
+        order.save()
+
+        messages.success(self.request, "Order was successful")
+        return redirect("/")
 
 
 class HomeView(ListView):
