f4851335df9cdcf6bb68091690aa991b9970ad11
Chris4891 <kolar.kristijan84@gmail.com>
2023-10-11 19:53:20 +0500

    Added Coupoinform in forms file

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 
+
+class CouponForm(forms.Form):
+    code = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Promo code'
+    }))
+
+
 class RefundForm(forms.Form):
     ref_code = forms.CharField()
     message = forms.CharField(widget=forms.Textarea(attrs={
