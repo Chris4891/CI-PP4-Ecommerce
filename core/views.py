d012f31d7224c2f303e3b0c397b6ff309bf8a982
Chris4891 <kolar.kristijan84@gmail.com>
2023-10-11 21:23:33 +0500

    Added exception handling for payment gateway

--- core/views.py
+++ core/views.py
@@ -40,28 +40,67 @@ class PaymentView(View):
         order = Order.objects.get(user=self.request.user, ordered=False)
         token = self.request.POST.get('stripeToken')
         amount = int(order.get_total() * 100)
-        
-        charge = stripe.Charge.create(
-            amount=amount,  # cents
-            currency="usd",
-            source=token
-        )
-        # create the payment
-        payment = Payment()
-        payment.stripe_charge_id = charge['id']
-        payment.user = self.request.user
-        payment.amount = order.get_total()
-        payment.save()
+        try:
+            charge = stripe.Charge.create(
+                amount=amount,  # cents
+                currency="usd",
+                source=token
+            )
+            # create the payment
+            payment = Payment()
+            payment.stripe_charge_id = charge['id']
+            payment.user = self.request.user
+            payment.amount = order.get_total()
+            payment.save()
 
-        # assign the payment to the order
-        order.ordered = True
-        order.payment = payment
-        # TODO : assign ref code
-        order.ref_code = create_ref_code()
-        order.save()
+            # assign the payment to the order
+            order.ordered = True
+            order.payment = payment
+            # TODO : assign ref code
+            order.ref_code = create_ref_code()
+            order.save()
 
-        messages.success(self.request, "Order was successful")
-        return redirect("/")
+            messages.success(self.request, "Order was successful")
+            return redirect("/")
+
+        except stripe.error.CardError as e:
+            # Since it's a decline, stripe.error.CardError will be caught
+            body = e.json_body
+            err = body.get('error', {})
+            messages.error(self.request, f"{err.get('message')}")
+            return redirect("/")
+
+        except stripe.error.RateLimitError as e:
+            # Too many requests made to the API too quickly
+            messages.error(self.request, "RateLimitError")
+            return redirect("/")
+
+        except stripe.error.InvalidRequestError as e:
+            # Invalid parameters were supplied to Stripe's API
+            messages.error(self.request, "Invalid parameters")
+            return redirect("/")
+
+        except stripe.error.AuthenticationError as e:
+            # Authentication with Stripe's API failed
+            # (maybe you changed API keys recently)
+            messages.error(self.request, "Not Authentication")
+            return redirect("/")
+
+        except stripe.error.APIConnectionError as e:
+            # Network communication with Stripe failed
+            messages.error(self.request, "Network Error")
+            return redirect("/")
+
+        except stripe.error.StripeError as e:
+            # Display a very generic error to the user, and maybe send
+            # yourself an email
+            messages.error(self.request, "Something went wrong")
+            return redirect("/")
+
+        except Exception as e:
+            # send an email to ourselves
+            messages.error(self.request, "Serious Error occured")
+            return redirect("/")
 
 
 class HomeView(ListView):
