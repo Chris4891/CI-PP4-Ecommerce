ad64d1548cd34ee76f5f865dcb9cbf1bc139110e
Chris4891 <kolar.kristijan84@gmail.com>
2023-10-11 21:53:23 +0500

    Created a class for Refund request view via get and post methods

--- core/views.py
+++ core/views.py
@@ -350,3 +350,36 @@ class AddCouponView(View):
                 return redirect("core:checkout")
 
 
+class RequestRefundView(View):
+    def get(self, *args, **kwargs):
+        form = RefundForm()
+        context = {
+            'form': form
+        }
+        return render(self.request, "request_refund.html", context)
+
+    def post(self, *args, **kwargs):
+        form = RefundForm(self.request.POST)
+        if form.is_valid():
+            ref_code = form.cleaned_data.get('ref_code')
+            message = form.cleaned_data.get('message')
+            email = form.cleaned_data.get('email')
+            # edit the order
+            try:
+                order = Order.objects.get(ref_code=ref_code)
+                order.refund_requested = True
+                order.save()
+
+                # store the refund
+                refund = Refund()
+                refund.order = order
+                refund.reason = message
+                refund.email = email
+                refund.save()
+
+                messages.info(self.request, "Your request was received")
+                return redirect("core:request-refund")
+
+            except ObjectDoesNotExist:
+                messages.info(self.request, "This order does not exist")
+                return redirect("core:request-refund")
