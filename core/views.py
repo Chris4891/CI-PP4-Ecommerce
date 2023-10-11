32ce09ef4cf723d595936415ad4f15b02fef5ff2
Chris4891 <kolar.kristijan84@gmail.com>
2023-10-11 21:52:19 +0500

    Created a class Add coupon view

--- core/views.py
+++ core/views.py
@@ -332,3 +332,21 @@ def get_coupon(request, code):
         return redirect("core:checkout")
 
 
+class AddCouponView(View):
+    def post(self, *args, **kwargs):
+        form = CouponForm(self.request.POST or None)
+        if form.is_valid():
+            try:
+                code = form.cleaned_data.get('code')
+                order = Order.objects.get(
+                    user=self.request.user, ordered=False)
+                order.coupon = get_coupon(self.request, code)
+                order.save()
+                messages.success(self.request, "Successfully added coupon")
+                return redirect("core:checkout")
+
+            except ObjectDoesNotExist:
+                messages.info(request, "You do not have an active order")
+                return redirect("core:checkout")
+
+
