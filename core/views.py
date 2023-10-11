3fd7e23aec63235eefe90afe3e74712b5b6901a5
Chris4891 <kolar.kristijan84@gmail.com>
2023-10-11 21:20:25 +0500

    Created a payment view

--- core/views.py
+++ core/views.py
@@ -1,5 +1,31 @@
-from django.views.generic import ListView
-from .models import Item
+from django.conf import settings
+from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, View
+from django.shortcuts import redirect
+from .models import Item, Order
+from django.shortcuts import render
+
+
+
+class PaymentView(View):
+    def get(self, *args, **kwargs):
+        # order
+        order = Order.objects.get(user=self.request.user, ordered=False)
+        if order.billing_address:
+            context = {
+                'order': order,
+                'DISPLAY_COUPON_FORM': False
+            }
+            return render(self.request, "payment.html", context)
+        else:
+            messages.warning(
+                self.request, "u have not added a billing address")
+            return redirect("core:checkout")
+
 
 class HomeView(ListView):
     template_name = "index.html"
