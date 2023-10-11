ca4d811a58e69ef14883bf91c50a6a0113ffad02
Chris4891 <kolar.kristijan84@gmail.com>
2023-10-11 19:43:35 +0500

    added the payment class in models

--- core/models.py
+++ core/models.py
@@ -167,3 +167,13 @@ class BillingAddress(models.Model):
         verbose_name_plural = 'BillingAddresses'
 
 
+class Payment(models.Model):
+    stripe_charge_id = models.CharField(max_length=50)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.SET_NULL, blank=True, null=True)
+    amount = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+
