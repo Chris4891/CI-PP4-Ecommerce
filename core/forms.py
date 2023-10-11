8afcaac5ed46b79b97054ff691b7e62f42eb0a10
Chris4891 <kolar.kristijan84@gmail.com>
2023-10-11 19:54:39 +0500

    Added a checkout form

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,21 @@
 from django import forms
 
+PAYMENT_CHOICES = (
+    ('S', 'Stripe'),
+    ('P', 'PayPal')
+)
+
+
+class CheckoutForm(forms.Form):
+    street_address = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': '1234 Main St',
+        'class': 'form-control'
+    }))
+    same_shipping_address = forms.BooleanField(required=False)
+    save_info = forms.BooleanField(required=False)
+    payment_option = forms.ChoiceField(
+        widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
+
 
 class CouponForm(forms.Form):
     code = forms.CharField(widget=forms.TextInput(attrs={
